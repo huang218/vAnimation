f7cc375c21a3b286a128ef5c5e07062d12e4627d
huangjh <848739492@qq.com>
2023-06-05 18:05:38 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 import { userStore } from '@/stores';
 import { useRouterJump } from '@/hooks/useRouterJump';
 
+const formLabelAlign = reactive({
+  name: '',
+  password: '',
+})
 const { replaceRouter } = useRouterJump();
 const { getUserInfo } = userStore();
 
@@ -11,11 +16,44 @@ async function submit() {
 }
 </script>
 <template>
-  <div>
-    <el-Button @click="submit">登录</el-Button>
+  <div class="login-box">
+    <div class="box">
+      <el-form
+        label-position="top"
+        label-width="100px"
+        :model="formLabelAlign"
+        style="max-width: 460px"
+      >
+        <el-form-item label="账号">
+          <el-input v-model="formLabelAlign.name" />
+        </el-form-item>
+        <el-form-item label="密码" typ>
+          <el-input v-model="formLabelAlign.password" />
+        </el-form-item>
+        <el-form-item>
+          <el-Button plain @click="submit">登录</el-Button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-
+.login-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box {
+    width: 300px;
+    height: 250px;
+    padding: 20px;
+    border-radius: 20px;
+    background-color: var(--el-bg-color);
+    :deep(.el-button) {
+      width: 100%;
+    }
+  }
+}
 </style>
