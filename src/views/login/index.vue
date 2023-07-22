79eacfa6b91271daf4d66dafa155d82322c5df37
huang218 <848739492@qq.com>
2023-07-22 23:19:16 +0800

    fix: login bg

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,6 +18,7 @@ async function submit() {
 <template>
   <div class="login-box">
     <div class="box">
+      <!-- <div class="text-center text-20px">Vue</div> -->
       <el-form
         label-position="top"
         label-width="100px"
@@ -25,10 +26,10 @@ async function submit() {
         style="max-width: 460px"
       >
         <el-form-item label="账号">
-          <el-input v-model="formLabelAlign.name" />
+          <el-input v-model="formLabelAlign.name" placeholder="请输入账号" />
         </el-form-item>
         <el-form-item label="密码" typ>
-          <el-input v-model="formLabelAlign.password" />
+          <el-input v-model="formLabelAlign.password" show-password placeholder="请输入密码" />
         </el-form-item>
         <el-form-item>
           <el-Button plain @click="submit">登录</el-Button>
@@ -45,12 +46,17 @@ async function submit() {
   display: flex;
   justify-content: center;
   align-items: center;
+  background-image: url(@/assets/image/login.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
   .box {
     width: 300px;
     height: 250px;
     padding: 20px;
     border-radius: 20px;
     background-color: var(--el-bg-color);
+    opacity: 0.8;
     :deep(.el-button) {
       width: 100%;
     }
