fe6fc48afa597a643102a4467e204490bca148fb
huang218 <848739492@qq.com>
2023-07-24 23:16:44 +0800

    fix

--- src/Layout/tags/index.vue
+++ src/Layout/tags/index.vue
@@ -205,7 +205,7 @@ onMounted(() => {
 </style>
 <style lang="less">
 .rightMenu {
-  @apply absolute top-0 left-0 z-999 text-14px h-auto p-4px rounded-4px overflow-hidden bg-light-50 shadow-md  dark: shadow-gray-800 dark: bg-dark-600;
+  @apply absolute top-0 left-0 z-999 text-14px h-auto p-4px rounded-6px overflow-hidden bg-light-50 shadow-md  dark: shadow-gray-800 dark: bg-dark-600;
   li {
     @apply h-25px text-center rounded-4px leading-25px cursor-pointer dark: text-light-500;
     &:hover {
