5ae8e6b48c2573ea2b1af02d6ddc57c189416793
huang218 <848739492@qq.com>
2024-04-09 22:29:01 +0800

    fix: 修改base前缀

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vAnimation/',
   plugins: [
     vue(),
     WindiCSS(),
