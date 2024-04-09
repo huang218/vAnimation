d3cd7f04cadf9dc45d06407ec6f3d031cd04355d
huang218 <848739492@qq.com>
2024-04-09 22:38:26 +0800

    fix: 修改环节配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vAnimation/',
+  base: import.meta.env.VITE_BASE_URL,
   plugins: [
     vue(),
     WindiCSS(),
